81f417f73f6b23b71fe5cad236365a1e10039df8
afraerol <afraaerol@gmail.com>
2023-06-12 20:25:37 +0300

    Update linkedList.java

---
+++ linkedList.java
@@ -0,0 +1,119 @@
+public class MyLinkedList {
+
+    public static void main(String[] args) {
+        MyLinkedList1 ml = new MyLinkedList1();
+        ml.mergeAll();
+        System.out.println(ml);
+        ml.insertFirst(1);
+        ml.mergeAll();
+        ml.insertFirst(2);
+        ml.mergeAll();
+        System.out.println(ml);
+        
+        ml.insertFirst(3);
+        ml.insertLast(5);
+        System.out.println(ml);
+        ml.mergeAll();
+        System.out.println(ml);
+        
+        System.out.println(ml);
+        ml.removeFirst();
+        System.out.println(ml);
+        
+    }
+    Node first, last;
+    int size = 0;
+
+    public MyLinkedList1() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Node removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node tmp = first;
+        first = first.next;
+        size--;
+        return tmp;
+    }
+    
+    public void mergeAll(){
+    }
+
+  
+   
+    public void insertFirst(int x) {
+        Node newNode = new Node(x);
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            newNode.next = first;
+            first = newNode;
+        }
+        size++;
+    }
+
+    public void insertAfter(Node p, int x) {
+        Node newNode = new Node(x);
+        if (p == null || size == 0) {
+            
+            return;
+        }
+        if (p==last){
+            insertLast(x);
+            return;
+        }
+        newNode.next = p.next;
+        p.next = newNode;
+        size++;
+    }
+
+   
+
+    public void insertLast(int x) {
+        Node newNode = new Node(x);
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
+    }
+    
+   
+
+    public String toString() {
+        if (isEmpty())
+            return "List empty";                    
+        Node tmp = first;
+        String str = "List with " + size + " elements: ";
+        
+        while (tmp != null) {
+            str += tmp.data + "->";
+            tmp = tmp.next;
+        }
+
+        return str;
+    }
+}
+
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
